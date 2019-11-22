4b0d0a330b106890fc4ace3ba81f4782e4849cf0
namu <yxmedia181004@gmail.com>
2019-11-22 11:35:12 +0900

    customs header screens

--- screens/PeriodSelecte.js
+++ screens/PeriodSelecte.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import {View, ImageBackground} from 'react-native';
-import {Icon, Text} from 'react-native-elements';
+import Icon from 'react-native-vector-icons/FontAwesome';
+import {Text} from 'react-native-elements';
 
 
 export default class PeriodSelecte extends React.Component{
@@ -11,15 +12,16 @@ export default class PeriodSelecte extends React.Component{
         return(
             <ImageBackground source={require('../assets/BG.png')} style={{width:'100%',height:'100%'}}>
             <View style={{flex:1,justifyContent:'center',alignItems:'center'}}>
-                <Text h1 style={{fontWeight:'800'}}>2年生</Text>
+                <Text h1 style={{fontWeight:'800',color:'#2E6392'}}>2年生</Text>
+                <View style={{flexDirection:'row', alignItems:'center',marginTop:50}}>
+                    <Icon name='calendar-check-o' size={30}/>
+                    <Text h4 style={{marginRight:50}}>2019-11-20</Text>
+                    <Text h4>水曜日</Text>
+                </View>
+            </View>
+            <View style={{flex:2}}>
 
             </View>
-            <View style={{flex:2,backgroundColor:'blue'}}>
-
-            </View>
-            {/* <View style={{flex:2,backgroundColor:'red'}}>
-
-            </View> */}
             </ImageBackground>
     );
     }
