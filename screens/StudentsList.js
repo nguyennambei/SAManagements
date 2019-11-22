e611298b5bfe4683ef4daf606a4802e2e3c13381
namu <yxmedia181004@gmail.com>
2019-11-22 13:31:45 +0900

    costums header name

--- screens/StudentsList.js
+++ screens/StudentsList.js
@@ -1,11 +1,17 @@
 import React from 'react';
 import {View, ImageBackground, Text} from 'react-native';
 
-export default function StudentsList (){
+export default class StudentsList extends React.Component{
+    static navigationOptions = ({navigation})=>{
+        return{
+            title: navigation.getParam('otherParam'),
+        };
+    }
+    render(){
     return(
-        <ImageBackground source={require('../assets/BG.png')} style={{width:'100%',height:'100%'}}>
-            <Text>Inside</Text>
+            <ImageBackground source={require('../assets/BG.png')} style={{width:'100%',height:'100%'}}>
             <Text>Inside</Text>
         </ImageBackground>
     );
-}
+    }
+} 
